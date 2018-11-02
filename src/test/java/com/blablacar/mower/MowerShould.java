c61643264fb599e86b8f82446bb5614c8a265e6b
snamdar <sepehr.namdar@ceetiz.com>
2018-11-02 16:20:44 +0100

    Cleaning the tests

--- src/test/java/com/blablacar/mower/MowerShould.java
+++ src/test/java/com/blablacar/mower/MowerShould.java
@@ -7,18 +7,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MowerShould {
 
-    private static final String NORTH = Orientation.NORTH;
-    private static final String EAST = Orientation.EAST;
-    private static final String LEFT = Move.LEFT;
-    private static final String RIGHT = Move.RIGHT;
-    private static final String FRONT = Move.FRONT;
-    private static final String WEST = Orientation.WEST;
+    private static final Orientation NORTH_ORIENTATION = Orientation.of(Orientation.NORTH);
+    private static final Orientation EAST_ORIENTATION = Orientation.of(Orientation.EAST);
+    private static final Orientation WEST_ORIENTATION = Orientation.of(Orientation.WEST);
+
+    private static final Move LEFT = Move.to(Move.LEFT);
+    private static final Move RIGHT = Move.to(Move.RIGHT);
+    private static final Move FRONT = Move.to(Move.FRONT);
 
     private Mower mower;
 
     @Before
     public void init() {
-        mower = new Mower(Orientation.of(NORTH));
+        mower = new Mower(NORTH_ORIENTATION);
     }
 
     @Test
@@ -31,12 +32,12 @@ public class MowerShould {
 
     @Test
     public void have_an_orientation() {
-        assertThat(mower.getOrientation()).isEqualToComparingFieldByField(Orientation.of(NORTH));
+        assertThat(mower.getOrientation()).isEqualToComparingFieldByField(NORTH_ORIENTATION);
     }
 
     @Test
-    public void understand_an_orientation() {
-        mower.moveTo(Move.to(FRONT));
+    public void move_front_when_receive_front_command() {
+        mower.moveTo(FRONT);
 
         Position expectedPosition = new Position(0, 1);
         assertThat(mower.getPosition()).isEqualToComparingFieldByField(expectedPosition);
@@ -44,20 +45,19 @@ public class MowerShould {
 
     @Test
     public void turn_to_right_when_receive_right_command() {
-        mower.moveTo(Move.to(RIGHT));
+        mower.moveTo(RIGHT);
 
         Position expectedPosition = new Position(0, 0);
         assertThat(mower.getPosition()).isEqualToComparingFieldByField(expectedPosition);
-        assertThat(mower.getOrientation()).isEqualTo(Orientation.of(EAST));
+        assertThat(mower.getOrientation()).isEqualTo(EAST_ORIENTATION);
     }
-
 
     @Test
     public void turn_to_left_when_receive_left_command() {
-        mower.moveTo(Move.to(LEFT));
+        mower.moveTo(LEFT);
 
         Position expectedPosition = new Position(0, 0);
         assertThat(mower.getPosition()).isEqualToComparingFieldByField(expectedPosition);
-        assertThat(mower.getOrientation()).isEqualTo(Orientation.of(WEST));
+        assertThat(mower.getOrientation()).isEqualTo(WEST_ORIENTATION);
     }
 }
